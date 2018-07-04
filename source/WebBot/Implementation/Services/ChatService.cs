b72ce5608e687948b5f5d04fae06cb4ef31779a9
Julian Hangstörfer <jhangstoerfer@gmail.com>
2018-07-04 20:23:12 +0200

    Add simple chat service

---
+++ source/WebBot/Implementation/Services/ChatService.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Bibby.Bot.Services
+{
+    class ChatService : IHostedService
+    {
+        private readonly DiscordSocketClient _discordClient;
+        private readonly ILogger _logger;
+
+        public ChatService(DiscordSocketClient discordClient, ILogger<ChatService> logger)
+        {
+            _discordClient = discordClient;
+            _logger = logger;
+        }
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _discordClient.MessageReceived += DiscordClientOnMessageReceived;
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _discordClient.MessageReceived -= DiscordClientOnMessageReceived;
+            return Task.CompletedTask;
+        }
+
+        private async Task DiscordClientOnMessageReceived(SocketMessage socketMessage)
+        {
+            _logger.LogInformation(socketMessage.ToString());
+            if (socketMessage.Content.ToLower().StartsWith("!ping"))
+            {
+                await socketMessage.Channel.SendMessageAsync("Pong");
+            }
+            if (socketMessage.Content.ToLower().StartsWith("!gdrp"))
+            {
+                await socketMessage.Channel.SendMessageAsync("I ain't saving shit.");
+            }
+        }
+    }
+}
