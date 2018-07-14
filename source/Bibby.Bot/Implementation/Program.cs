c8c22e0e2d17eb560dfab8293dd14c809d55aa02
Julian Hangstörfer <jhangstoerfer@gmail.com>
2018-07-14 23:41:12 +0200

    Convert back to asp.net core web host

--- source/Bibby.Bot/Implementation/Program.cs
+++ source/Bibby.Bot/Implementation/Program.cs
@@ -1,62 +1,82 @@
-﻿using System.Threading.Tasks;
+﻿using System.IO;
+using System.Threading.Tasks;
 using Bibby.Bot.Options;
 using Bibby.Bot.Services;
 using Discord;
 using Discord.Rest;
 using Discord.WebSocket;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using EnvironmentName = Microsoft.Extensions.Hosting.EnvironmentName;
 
 namespace Bibby.Bot
 {
     public class Program
     {
-        public static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
-            var host = new HostBuilder()
-                .ConfigureAppConfiguration(ConfigureAppConfiguration)
-                .ConfigureServices(ConfigureServices)
-                .ConfigureLogging(ConfigureLogging)
-                .UseConsoleLifetime()
-                .UseEnvironment(EnvironmentName.Development)
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile("secrets.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
                 .Build();
 
-            await host.RunAsync();
+            CreateWebHostBuilder(args, config).Build().Run();
         }
 
-        private static void ConfigureAppConfiguration(HostBuilderContext context, IConfigurationBuilder config)
-        {
-            config
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange:true)
-                .AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true)
-                .AddJsonFile("secrets.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"secrets.{context.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true)
-                .AddEnvironmentVariables();
-        }
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args, IConfiguration config) =>
+            WebHost.CreateDefaultBuilder(args)
+            .UseConfiguration(config)
+            .UseStartup<Startup>();
+        //public static async Task Main(string[] args)
+        //{
+        //    var host = new HostBuilder()
+        //        .ConfigureAppConfiguration(ConfigureAppConfiguration)
+        //        .ConfigureServices(ConfigureServices)
+        //        .ConfigureLogging(ConfigureLogging)
+        //        .UseConsoleLifetime()
+        //        .UseEnvironment(EnvironmentName.Development)
+        //        .Build();
 
-        private static void ConfigureLogging(HostBuilderContext context, ILoggingBuilder logging)
-        {
-            logging.AddConsole();
-            logging.AddDebug();
-        }
+        //    await host.RunAsync();
+        //}
 
-        private static void ConfigureServices(HostBuilderContext context, IServiceCollection services)
-        {
-            services.AddLogging();
-            services.Configure<DiscordOptions>(context.Configuration.GetSection("DiscordOptions"));
-            var discordClient = new DiscordSocketClient();
+        //private static void ConfigureAppConfiguration(HostBuilderContext context, IConfigurationBuilder config)
+        //{
+        //    config
+        //        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+        //        .AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+        //        .AddJsonFile("secrets.json", optional: true, reloadOnChange: true)
+        //        .AddJsonFile($"secrets.{context.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+        //        .AddEnvironmentVariables();
+        //}
 
-            services.AddSingleton<IDiscordClient>(discordClient);
-            services.AddSingleton<BaseDiscordClient>(discordClient);
-            services.AddSingleton(discordClient);
+        //private static void ConfigureLogging(HostBuilderContext context, ILoggingBuilder logging)
+        //{
+        //    logging.AddConsole();
+        //    logging.AddDebug();
+        //}
 
-            services.AddHostedService<LifetimeEventsHostedService>();
-            services.AddHostedService<LogService>();
-            services.AddHostedService<LoginService>();
-            services.AddHostedService<ChatService>();
-            services.AddHostedService<TemperatureService>();
-        }
+        //private static void ConfigureServices(HostBuilderContext context, IServiceCollection services)
+        //{
+        //    services.AddLogging();
+        //    services.Configure<DiscordOptions>(context.Configuration.GetSection("DiscordOptions"));
+        //    var discordClient = new DiscordSocketClient();
+
+        //    services.AddSingleton<IDiscordClient>(discordClient);
+        //    services.AddSingleton<BaseDiscordClient>(discordClient);
+        //    services.AddSingleton(discordClient);
+
+        //    services.AddHostedService<LifetimeEventsHostedService>();
+        //    services.AddHostedService<LogService>();
+        //    services.AddHostedService<LoginService>();
+        //    services.AddHostedService<ChatService>();
+        //    services.AddHostedService<TemperatureService>();
+        //}
     }
 }
---
+++ source/Bibby.Bot/Implementation/Startup.cs
@@ -0,0 +1,44 @@
+﻿using Bibby.Bot.Options;
+using Bibby.Bot.Services;
+using Discord;
+using Discord.Rest;
+using Discord.WebSocket;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Bibby.Bot
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLogging();
+            services.Configure<DiscordOptions>(Configuration.GetSection("DiscordOptions"));
+
+            var discordClient = new DiscordSocketClient();
+            services.AddSingleton<IDiscordClient>(discordClient);
+            services.AddSingleton<BaseDiscordClient>(discordClient);
+            services.AddSingleton(discordClient);
+
+            services.AddHostedService<LifetimeEventsHostedService>();
+            services.AddHostedService<LogService>();
+            services.AddHostedService<LoginService>();
+            services.AddHostedService<ChatService>();
+            services.AddHostedService<TemperatureService>();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+        }
+    }
+}
