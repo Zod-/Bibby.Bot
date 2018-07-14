933632f17b7615197994327ff261c2c8f525ec6b
Julian Hangstörfer <jhangstoerfer@gmail.com>
2018-07-14 19:46:13 +0200

    Add temperature mention class

---
+++ source/Bibby.Bot/Implementation/Utilities/TemperatureMention.cs
@@ -0,0 +1,15 @@
+﻿namespace Bibby.Bot.Utilities
+{
+    public class TemperatureMention
+    {
+        public float Degrees { get; set; }
+        public TemperatureUnit TemperatureUnit { get; set; }
+    }
+
+    public enum TemperatureUnit
+    {
+        Celsius,
+        Fahrenheit,
+        Kelvin
+    }
+}
