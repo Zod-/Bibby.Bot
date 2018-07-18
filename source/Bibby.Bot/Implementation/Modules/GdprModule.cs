b8b6e901c3a278ba021e8a13767ff9f36700d547
Julian Hangstörfer <jhangstoerfer@gmail.com>
2018-07-18 17:43:47 +0200

    Move gdpr command to own module

---
+++ source/Bibby.Bot/Implementation/Modules/GdprModule.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Bibby.Bot.Utilities.Extensions;
+using Discord.Commands;
+
+namespace Bibby.Bot.Services
+{
+    public class GdprModule : ModuleBase<ICommandContext>
+    {
+        public IMessageService MessageService { get; set; }
+
+        [Command("gdpr")]
+        public async Task RunAsync()
+        {
+            await MessageService.SendAsync(Context.Channel, "I ain't saving shit.").DeleteAfterSeconds(15);
+        }
+    }
+}
