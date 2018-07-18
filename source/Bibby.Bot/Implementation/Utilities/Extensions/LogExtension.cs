371cd4bae91b82279fe1907f48555052aa60b633
Julian Hangstörfer <jhangstoerfer@gmail.com>
2018-07-18 17:43:17 +0200

    Add converter for discord logger to microsoft logger

---
+++ source/Bibby.Bot/Implementation/Utilities/Extensions/LogExtension.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using Discord;
+using Microsoft.Extensions.Logging;
+
+namespace Bibby.Bot.Utilities.Extensions
+{
+    public static class LogExtension
+    {
+        public static void Log(this ILogger logger, LogMessage logMessage)
+        {
+            logger.Log(logMessage.Severity.ToLogLevel(), logMessage.Exception, logMessage.Message, logMessage.Source);
+        }
+
+        public static LogLevel ToLogLevel(this LogSeverity logSeverity)
+        {
+            switch (logSeverity)
+            {
+                case LogSeverity.Critical:
+                    return LogLevel.Critical;
+                case LogSeverity.Error:
+                    return LogLevel.Error;
+                case LogSeverity.Warning:
+                    return LogLevel.Warning;
+                case LogSeverity.Info:
+                    return LogLevel.Information;
+                case LogSeverity.Verbose:
+                    return LogLevel.Trace;
+                case LogSeverity.Debug:
+                    return LogLevel.Debug;
+                default:
+                    return LogLevel.Information;
+            }
+
+        }
+    }
+}
---
+++ source/Bibby.Bot/Tests/Utilities/Extensions/LogLevelExtensions.cs
@@ -0,0 +1,23 @@
+﻿
+using Bibby.Bot.Utilities.Extensions;
+using Discord;
+using Microsoft.Extensions.Logging;
+using NUnit.Framework;
+
+namespace Bibby.Bot.Tests.Utilities.Extensions
+{
+    public class LogLevelExtensions
+    {
+        [TestCase(LogLevel.Error, LogSeverity.Error)]
+        [TestCase(LogLevel.Critical, LogSeverity.Critical)]
+        [TestCase(LogLevel.Trace, LogSeverity.Verbose)]
+        [TestCase(LogLevel.Information, LogSeverity.Info)]
+        [TestCase(LogLevel.Warning, LogSeverity.Warning)]
+        [TestCase(LogLevel.Debug, LogSeverity.Debug)]
+        public void ConvertLoglevelTests(LogLevel expected, LogSeverity input)
+        {
+            var actual = input.ToLogLevel();
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
