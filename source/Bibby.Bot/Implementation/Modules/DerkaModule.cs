8778ec80e3d81812195ac021a10f5906fa4a2cf5
Julian Hangstörfer <jhangstoerfer@gmail.com>
2018-07-19 14:58:32 +0200

    Add translation module

---
+++ source/Bibby.Bot/Implementation/Modules/DerkaModule.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Bibby.Bot.Services;
+using Bibby.Bot.Services.Translations;
+using Discord.Commands;
+
+namespace Bibby.Bot.Modules
+{
+    public class DerkaModule : ModuleBase<ICommandContext>
+    {
+        public ITranslator Translator { get; set; }
+        public IMessageService MessageService { get; set; }
+
+        [Command("derka")]
+        [Alias("d", "translate")]
+        public async Task RunAsync(params string[] words)
+        {
+            var text = string.Join(" ", words);
+            var response = await Translator.DetectAndTranslateAsync(text);
+            await MessageService.SendAsync(Context.Channel, response);
+        }
+    }
+}
